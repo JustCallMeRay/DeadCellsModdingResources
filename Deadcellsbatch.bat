72d6665012d77100dd5733c61c417c0b2bb15491
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2019-11-10 10:19:18 +0000

    Changed some things

--- Deadcellsbatch.bat
+++ Deadcellsbatch.bat
@@ -1,6 +1,6 @@
 @echo off
 
-Call DeadCellsPaths.bat 
+@Call DeadCellsPaths.bat 
 
 @rem echo %Root%
 CD %ModTools%
@@ -10,22 +10,24 @@ echo In ModTools
 :ThePAKTool
 CHOICE /C YN /M "Do you want to run PAKTool?"
 IF ERRORLEVEL 2 goto TheCDBTool
-IF ERRORLEVEL 1 echo Running PAKTool
+IF ERRORLEVEL 1 echo Running PAKTool, this may take some time
 echo on 
 PAKTool -Expand -OutDir %Resources%\ResPAK\ -RefPak "%Root%\res.pak" 
 @echo off
+echo Done, press any key to contiue 
+@pause
 goto TheCDBTool
 
 
 :TheCDBTool 
 CHOICE /M "Do you want to run CDBTool?"
 IF ERRORLEVEL 2 goto End
-IF ERRORLEVEL 1 echo Running CDBTool
+IF ERRORLEVEL 1 echo Running CDBTool, this may take some time
 echo on
 CDBTool -Expand -OutDir %Resources%\ExpandedCDB\ -RefCDB "%Resources%\ResPAK\data.cdb"
 @echo off
-if errorlevel 1 echo unsuccessfull
-echo "Ran CDBTool"
+echo Done, press any key to contiue 
+@pause
 
 :End
 Pause
