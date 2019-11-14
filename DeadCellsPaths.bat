b532276d68d105cc695a3f2d303afb948471330a
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2019-11-14 15:43:45 +0000

    Lots of changes
    Stuff works now 
    Atlas comming soon... (eventually)

--- DeadCellsPaths.bat
+++ DeadCellsPaths.bat
@@ -1,12 +1,19 @@
+@echo off
+
+:Info
+echo You will have to edit this file. Use notepad, notepad++ or an IDE
+echo. 
 echo Root folder is where your dead cells.exe file is stored
 echo ModTools folder comes with dead cells, if you haven't moved it leave as default. 
 echo Resources is the folder you want the unpacked files to be placed in 
-echo DO NOT use quoteation marks, it will not work
+echo. 
+echo DO NOT use quoteation or double quotes, it will not work
+Echo.  
+echo You DO NOT have to open this file in cmd prompt
 
+:Variables
 set Root=C:\Program Files (x86)\Steam\steamapps\common\Dead Cells
 set ModTools=%Root%\ModTools
-set Resources=C:\Users\Ray\Documents\DeadcellsModding
-
-
-
-
+set Resources=C:\Users\You!!!!\Documents\DeadcellsModding
+set Output=%Resources%\PackedData
+echo var done
--- Deadcellsbatch.bat
+++ Deadcellsbatch.bat
@@ -1,36 +1,103 @@
-@echo off
+REM @echo off
 
 @Call DeadCellsPaths.bat 
-
+cls
 @rem echo %Root%
 CD %ModTools%
-echo %CD%
-echo In ModTools
+REM echo %CD%
+REM echo In ModTools
+:Start
+Choice /C UARP /M "Do you want to Unpak/cdbExpand, AtlasTool, Repack or view Paths?"
+If errorlevel 4 goto Options
+If errorlevel 3 goto Repack
+If errorlevel 2 goto Atlas
+If errorlevel 1 goto UnPAKTool
 
-:ThePAKTool
-CHOICE /C YN /M "Do you want to run PAKTool?"
-IF ERRORLEVEL 2 goto TheCDBTool
-IF ERRORLEVEL 1 echo Running PAKTool, this may take some time
+:UnPAKTool
+CHOICE /C YN /M "Do you want to run PAKTool? (UNPACK)"
+IF ERRORLEVEL 2 goto CDBExpand
+IF ERRORLEVEL 1 echo. 
+echo Running PAKTool, this may take some time
 echo on 
 PAKTool -Expand -OutDir %Resources%\ResPAK\ -RefPak "%Root%\res.pak" 
 @echo off
 echo Done, press any key to contiue 
-@pause
-goto TheCDBTool
+@pause >nul
+goto CDBExpand
 
 
-:TheCDBTool 
-CHOICE /M "Do you want to run CDBTool?"
+:CDBExpand
+Choice /M "Do you want to run CDBTool? (EXPAND)"
 IF ERRORLEVEL 2 goto End
-IF ERRORLEVEL 1 echo Running CDBTool, this may take some time
+IF ERRORLEVEL 1 echo. 
+echo Running CDBTool, this may take some time
 echo on
 CDBTool -Expand -OutDir %Resources%\ExpandedCDB\ -RefCDB "%Resources%\ResPAK\data.cdb"
 @echo off
 echo Done, press any key to contiue 
-@pause
+@pause >nul
+goto End
+
+:Atlas
+echo Not yet supported push any button 
+@pause >nul
+goto Start
+
+:Repack
+:CBDCollapse
+echo "Do you want to run CDBTool? (COLLAPSE)"
+Choice /M "Replaces original cdb file"
+IF ERRORLEVEL 2 goto RePAKTool
+IF ERRORLEVEL 1 echo Running CDBTool, this may take some time
+echo on
+CDBTool -Collapse -InDir %Resources%\ExpandedCDB\ -OutCDB "%Resources%\ResPAK\data.cdb" 
+@echo off
+echo Done, press any key to contiue 
+@pause >nul
+goto RePAKTool
+
+:RePAKTool
+CHOICE /M "Do you want to run PAKTool? (REPACK)"
+IF ERRORLEVEL 2 goto End
+IF ERRORLEVEL 1 echo.
+Echo Running PAKTool, this may take some time...
+Echo.
+echo on 
+PAKTool -CreateDiffPak -RefPak "%Root%\res.pak" -InDir "%Resources%\ResPAK" -OutPak "%Output%\res.pak" 
+@echo off
+echo Done, press any key to contiue 
+@pause >nul
+goto End
+
+:Options
+Echo ALPHA
+Choice /M "Show paths?"
+if ERRORLEVEL 2 goto Options1
+Echo Show paths Resources ModTools rooT Output
+@CHOICE /C RMTO
+if errorlevel 4 echo %Output%
+if errorlevel 3 echo %Root%
+if errorlevel 2 echo %ModTools%
+if errorlevel 1 echo %Resources%
+:Options1
+Echo Thats all folks go to
+Choice /C ES /M "End or Start?"
+if errorlevel 2 goto Start
+if errorlevel 1 goto End
 
 :End
-Pause
-@echo on
+Echo. 
+Echo Completed...
+echo. 
+Echo Exit
+echo Open cmd at ModTools or
+Echo Re-Run the program
+@Choice /C EOR
+If errorlevel 3 goto start
+If errorlevel 2 goto OpenCMD
+if errorlevel 1 exit 
 
-@rem cmd /k
+:OpenCMD
+Cd %ModTools%
+CMD /K
+
